a3af619debe3574f490e46d3bb46d430c8ce7cbe
ELIZA <rysaeva96@mail.ru>
2022-06-07 14:02:23 +0500

    Задача с заменой первой и последней строк массива

---
+++ Task_53/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив. Напишите программу, которая 
+// поменяет первую и последнюю строку массива
+
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(10);
+
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+int[,] ChangeRowArray(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        temp = array[0,i];
+        array[0,i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) -1, i] = temp;
+    }
+    return array;
+}
+
+int[,] array = GetArray(rows, cols);
+Console.WriteLine("Массив: ");
+PrintArray(array);
+int[,] newarray = ChangeRowArray(array);
+Console.WriteLine("Массив с заменой 1 и последней строк: ");
+PrintArray(newarray);
